022b2c4bfb0481896812ac888894c05341e41e5f
GENERICO\usuario <marciovf95@gmail.com>
2017-05-25 17:00:40 -0300

    Debater Sobre a Funcao

--- autentica.php
+++ autentica.php
@@ -10,7 +10,7 @@
 	var_dump($_POST);
 	if(isset($_POST)){
 
-		$pessoa->pesCpf = addslashes(Mascaras::removeMascara($_POST['cpf']));
+		$pessoa->pesCpf = addslashes(Mascaras::removeMascara($_POST['cpf']);
 		$pessoa->pesSenha = addslashes(sha1($_POST['senha']));
 
 		$resultado = $pessoa->logar();
@@ -34,4 +34,17 @@
 			header('location: index.php');
 		}
 	}
+
+			public function anti_injection($sql)
+		{
+			//debater com o pessoal se Conven Colocar
+		// remove palavras que contenham sintaxe sql
+		$sql = preg_replace(sql_regcase("/(from|select|insert|delete|where|drop table|show tables|#|\*|--|\\\\)/"),"",$sql);
+		$sql = trim($sql);//limpa espaços vazio
+		$sql = strip_tags($sql);//tira tags html e php
+		$sql = addslashes($sql);//Adiciona barras invertidas a uma string
+		return $sql;
+		}
+
+
 ?>
