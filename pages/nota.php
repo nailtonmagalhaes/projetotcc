7d1f4ac06995e2c8bb14c7b596f751bfcdcb6ada
Nailton <nailton85@gmail.com>
2017-06-05 18:10:50 -0300

    nota

--- pages/nota.php
+++ pages/nota.php
@@ -24,11 +24,39 @@
         }
 
         public function carregarDados(){
-
+            try{
+                $resultado = AcessoDados::listar("SELECT Id, IdProfessor, IdAvaliacao, Nota, Ativo FROM tbNota WHERE Id = {$this->notId}");
+                if($resultado && $resultado->num_rows > 0){
+                    $row = $resultado->fetch_assoc();
+                    $this->notNota = $row['Nota'];
+                    $this->notAtivo = $row['Ativo'];
+                    $this->notProfessor = new Professor();
+                    $this->notProfessor->pesId = $row['IdProfessor'];
+                    $this->notProfessor->carregarDados();
+                    $this->notAvaliacao = new Avaliacao();
+                    $this->notAvaliacao = $row['IdAvaliacao'];
+                    $this->notAvaliacao->carregarDados();
+                    return true;
+                }
+                return false;
+            }catch(Exception $ex){
+                throw new Exception("Ocorreu um erro ao carregar os dados da nota.\n{$ex->getMessage()}");
+            }
         }
 
         public function salvarDados(){
-
+             try{
+                AcessoDados::abreTransacao();
+                if($this->notId > 0){
+                    AcessoDados::alterar("");
+                }else{
+                    AcessoDados::inserir("");
+                }
+                AcessoDados::confirmaTransacao();
+                return true;
+            }catch(Exception $ex){
+                throw new Exception("Ocorreu um erro ao carregar os dados da nota.\n{$ex->getMessage()}");
+            }
         }
     
         public function excluirLogicamente(){
